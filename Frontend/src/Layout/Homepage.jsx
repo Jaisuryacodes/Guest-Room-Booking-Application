da2afd97550bb5f38641a0b070af57f2d449b4b3
Jaisuryacodes <jaisuryasjv2@gmail.com>
2024-05-24 09:03:06 +0530

    fix :fixed some alignment

--- Frontend/src/Layout/Homepage.jsx
+++ Frontend/src/Layout/Homepage.jsx
@@ -4,21 +4,22 @@ import Footer from '../components/Footer'
 import Content from '../components/Content.jsx'
 import Menu from '../components/Menu'
 import { Outlet } from 'react-router-dom'
+import Checkbox from '../components/Checkbox.jsx'
 const Homepage = () => {
   return (
     <>
-   <Menu/>
-   <div className="px-[40px] py-5">
-   <Outlet/>
+      <Menu />
+
+      <div className="px-[40px] py-5">
+        <Outlet />
+        <Checkbox />
+
+       
+      </div>
 
     
-   <Content/>
-
-   </div>
-    
-    <Footer/>
-   </>
-  )
+    </>
+  );
 }
 
 export default Homepage
